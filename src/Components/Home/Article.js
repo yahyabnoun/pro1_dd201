3b99073267973d79ead08d3568c69f54bcffc803
yahya bnoun <yahyaiko22@gmail.com>
2022-10-28 21:48:31 +0100

    initial commit

--- src/Components/Home/Article.js
+++ src/Components/Home/Article.js
@@ -2,11 +2,55 @@ import React from 'react'
 import './style.css'
 
 
-const Article = () =>{
+const Article = () => {
   return (
-    <div className="Article">
-        <h2 >Article</h2>
-    </div>
+    <>
+        <table id="customers">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Berglunds snabbköp</td>
+              <td>Christina Berglund</td>
+              <td>Sweden</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Königlich Essen</td>
+              <td>Philip Cramer</td>
+              <td>Germany</td>
+            </tr>
+          </tbody>
+
+
+
+        </table>
+    </>
+
   );
 }
 
